c23fbe83eb5333cae02a258d6a49d81aa2b46b85
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-14 00:00:04 +0530

    do

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,4 +3,5 @@ import { CartProvider } from "../contexts/context";
 export const Cart = () => {
   const value = useContext(CartProvider);
   return <h1>this is {value}</h1>;
+  console.log("here");
 };
