6e83c7e6edf3c4529079e57eace8e3fab9c27af5
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-13 23:57:55 +0530

    get

--- src/contexts/context.jsx
+++ src/contexts/context.jsx
@@ -1,7 +1,7 @@
-import { createContext } from "react";
+// import { createContext } from "react";
 
-export const CartProvider = createContext();
+// export const CartProvider = createContext();
 
-export const Createcartprovider = ({ children }) => {
-  <CartProvider.Provider value={10}> {children} </CartProvider.Provider>;
-};
+// export const Createcartprovider = ({ children }) => {
+//   <CartProvider.Provider value={10}> {children} </CartProvider.Provider>;
+// };
