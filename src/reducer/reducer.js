6ff224d968a4ed018ae531178b3f12007e80889e
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-13 23:55:31 +0530

    store

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,11 +1,11 @@
-import { ADD_COUNT } from "./action";
+// import { ADD_COUNT } from "./action";
 
-export const reducer = (store, { type, payload }) => {
-  console.log(store);
-  switch (type) {
-    case ADD_COUNT:
-      return { ...store, count: store.count + payload };
-    default:
-      return store;
-  }
-};
+// export const reducer = (store, { type, payload }) => {
+//   console.log(store);
+//   switch (type) {
+//     case ADD_COUNT:
+//       return { ...store, count: store.count + payload };
+//     default:
+//       return store;
+//   }
+// };
